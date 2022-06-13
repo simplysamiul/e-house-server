cd496e10714399c9153a5ba56a2a6d1533a2c007
Md Samiul <simplysamiul@gmail.com>
2022-06-13 11:10:54 +0600

    update code

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ async function run(){
         app.get("/shipping", async(req,res)=>{
             const email = req.query.email;
             const query = {email};
-            const cursor = shippingCollection.findOne(query);
+            const cursor = shippingCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
